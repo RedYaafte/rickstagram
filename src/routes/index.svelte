dc13f503b08dfcf56fa45f4cccc5298834f0bdf6
Yafté M <redyafte@gmail.com>
2021-09-16 13:46:20 -0500

    Feat: added sapper

---
+++ src/routes/index.svelte
@@ -0,0 +1,27 @@
+<script context="module">
+  const RICK = "https://rickandmortyapi.com/api/character/1";
+  const API =
+    "https://rickandmortyapi.com/api/character/1,8,15,19,22,48,56,69,72,74,78,82,86,103,119";
+
+  export async function preload() {
+    const data = await this.fetch(API).then((r) => r.json());
+    return { data };
+  }
+</script>
+
+<script>
+  import Main from "../components/Main.svelte";
+  import TimeLine from "../components/TimeLine.svelte";
+  import Sidebar from "../components/Sidebar.svelte";
+
+  export let data;
+</script>
+
+<svelte:head>
+  <title>Rickstagram</title>
+</svelte:head>
+
+<Main>
+  <TimeLine posts={data} />
+  <Sidebar results={data.results} />
+</Main>
